98b8fb4d5a259377031d5f20b812b55ae659b163
Vladimir Olennikov <ath31st@yandex.ru>
2023-12-08 18:41:21 +0300

    fix security config

--- src/main/java/home/sweethome/atf/config/SecurityConfig.java
+++ src/main/java/home/sweethome/atf/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -18,12 +19,15 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
-
-        .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/register", "/css/**").permitAll();
+            auth.requestMatchers("/", "/users", "/new-user").hasRole("USER");
+            auth.anyRequest().authenticated();
+        })
         .httpBasic(withDefaults())
-        .formLogin(withDefaults())
-        .securityMatcher("/register", "/css/**")
-        .securityMatcher("/", "/users", "/new-user");
+        .formLogin(withDefaults());
 
     return httpSecurity.build();
   }
