4fde38a17f7670f0f339eaf3eb04b29e302655e5
Vladimir Olennikov <ath31st@yandex.ru>
2023-12-13 16:52:00 +0300

    update change background function to automatic

--- src/main/resources/static/js/colorBackgroundChanger.js
+++ src/main/resources/static/js/colorBackgroundChanger.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
-    // Функция для изменения цвета фона
     function changeBackgroundColor() {
-        const colors = ['#ff9999', '#99ff99', '#9999ff', '#ffff99', '#ffcc99'];
+        const colors = [
+            '#ff9999', '#99ff99', '#9999ff', '#ffff99', '#ffcc99',
+            '#ff6666', '#66ff66', '#6666ff', '#ffff66', '#ff9966',
+            '#ff3333', '#33ff33', '#3333ff', '#ffff33', '#ff6633'
+        ];
         const randomColor = colors[Math.floor(Math.random() * colors.length)];
         $('body').css('background-color', randomColor);
     }
 
-    $('#changeColorButton').click(function () {
+    setInterval(function () {
         changeBackgroundColor();
-    });
+    }, 2000);
 });
