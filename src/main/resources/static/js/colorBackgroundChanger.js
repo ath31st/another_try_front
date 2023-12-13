9f0d64f7f8e59afcc94804c176eba3807deb1dff
Vladimir Olennikov <ath31st@yandex.ru>
2023-12-13 17:05:00 +0300

    add running-line text

--- src/main/resources/static/js/colorBackgroundChanger.js
+++ src/main/resources/static/js/colorBackgroundChanger.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         const colors = [
             '#ff9999', '#99ff99', '#9999ff', '#ffff99', '#ffcc99',
             '#ff6666', '#66ff66', '#6666ff', '#ffff66', '#ff9966',
-            '#ff3333', '#33ff33', '#3333ff', '#ffff33', '#ff6633'
+            '#d07a7a', '#299b29', '#262686', '#b0b02d', '#ff6633'
         ];
         const randomColor = colors[Math.floor(Math.random() * colors.length)];
         $('body').css('background-color', randomColor);
