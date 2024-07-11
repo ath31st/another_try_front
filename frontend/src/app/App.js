45a3fa9d6d550411c8d92eb620546ddbe8399363
ath31st <ath31st@yandex.ru>
2024-07-11 09:24:21 +0300

    add header widget

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,9 +1,11 @@
 import './styles/App.css';
 import Footer from '../widgets/footer/footer';
+import Header from '../widgets/header/header';
 
 function App() {
   return (
       <div className="App">
+        < Header/>
         <p>Test</p>
         < Footer/>
       </div>
---
+++ frontend/src/widgets/header/header.jsx
@@ -0,0 +1,14 @@
+import './header.css';
+import Navbar from '../navbar/navbar';
+
+const Header = () => {
+  return (
+      <div>
+        <title>Fullstack app</title>
+        <link rel="icon" href="/favicon.ico"/>
+        < Navbar/>
+      </div>
+  );
+};
+
+export default Header;
